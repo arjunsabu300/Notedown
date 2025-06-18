a5c89cf49d2c2aae3b29f2a9d8cc2a311dc760b3
arjunsabu300 <arjunsabumark32@gmail.com>
2025-06-18 22:55:30 +0530

    changes

--- Notedown_Project/src/backend/app.js
+++ Notedown_Project/src/backend/app.js
@@ -6,6 +6,7 @@ const Users = require('./models/User');
 const Task = require('./models/Tasks');
 const startreminder = require('./Message');
 const axios = require('axios');
+const twilio = require('twilio');
 
 dotenv.config();
 const app = express();
@@ -24,7 +25,7 @@ async function main() {
   });
 }
 
-startreminder();
+
 
 app.post('/api/register', async (req, res) => {
   const { username, password,phonenumber } = req.body;
@@ -93,6 +94,65 @@ app.delete('/api/tasks/:id', async (req, res) => {
 app.get('/healthcheck', (req, res) => {
   res.status(200).send('OK');
 });
+
+
+
+
+
+const accountSid = process.env.Twilio_sid;
+const authToken = process.env.Twilio_auth_token;
+
+const client = twilio(accountSid,authToken);
+
+const FROM_NUMBER= process.env.Twilio_from_number;
+
+
+const sendWhatsappreminder = async(task,tophonenumber)=>{
+    try {
+        await client.messages.create({
+            from: FROM_NUMBER,
+            to: `whatsapp:+91${tophonenumber}`,
+            
+            body: `🔔 Reminder: You have an upcoming task "${task.title}" for subject "${task.subject}" due on ${task.dueDate}. Don't forget to finish it!`,
+
+        });
+        console.log('reminder sent for task:',task.title);
+        console.log(tophonenumber);
+        
+    } catch (error) {
+        console.error('Error sending reminder', error);
+    }
+};
+
+
+app.get('/send-whatsapp-reminders',async(req,res)=>{
+  try{
+        const now = new Date();
+        const threedayslater = new Date(now);
+        threedayslater.setDate(now.getDate()+2);
+        console.log(threedayslater);
+
+        const datestr = threedayslater.toISOString().split('T')[0];
+        console.log(datestr);
+        const tasks = await Task.find({dueDate: datestr,completed:false});
+
+        for(const task of tasks){
+            const user = await Users.findById(task.userId);
+            const userphonenumber = user.phonenumber
+            if(userphonenumber){
+                await sendWhatsappreminder(task,userphonenumber);
+            }
+            else{
+                console.log('No phonenumber found');
+            }
+        }
+        res.send(`✅ Sent WhatsApp reminders for tasks due on ${datestr}`);
+  }catch(error){
+      console.error('Error sending reminders:', error);
+      res.status(500).json({ error: 'Failed to send reminders' });
+  }
+});
+
 
 
 
